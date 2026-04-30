db04c4d77f9ddc79336b2a231d206376e2912600
josegab-tech <jose.gabriel.tech@gmail.com>
2026-04-29 22:15:59 -0300

    slide 200 part 1

--- arquitetura-camada/src/Model/Usuario.java
+++ arquitetura-camada/src/Model/Usuario.java
@@ -3,14 +3,26 @@ package Model;
 public class Usuario{
     private String nome;
     private String email;
+    private String telefone;
 
     public Usuario(String nome, String email){
         this.nome = nome;
         this.email = email;
+        this.telefone = null;
+    }
+
+    public Usuario(String nome, String email, String telefone){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
     }
 
     public String getNome(){
         return this.nome;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
     }
 
     public String getEmail(){
@@ -19,6 +31,10 @@ public class Usuario{
 
     public void setNome(String nome){
         this.nome =nome;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
     }
 
     public void setEmail(String email){
